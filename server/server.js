9b5a0d6e437032d9801f1e9aa8954f2af8b8de74
B. Abhilash <abhilash.bagadi@rampgroup.com>
2026-03-21 16:59:33 +0530

    Ready to deploy

--- server/server.js
+++ server/server.js
@@ -9,4 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (require.main === module) {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
